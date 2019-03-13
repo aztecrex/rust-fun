b2c045405c012823b9c490d2c0e70a8a64cb54a6
Greg Wiley <gwiley@cj.com>
2019-03-13 09:38:12 -0700

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,19 @@ fn main() {
     t(&mut a);
     println!("{}", a);
     println!("{}", u(&a));
+
+    let b1 = String::from("left");
+    let b2 = String::from("right");
+    let c = b1 + &b2;
+
+    println!("{}", c);
+
+    panic!("this don't work");
+
 }
 
 fn t( v:  &mut i32) {
-    *v = *v + 1;
+    *v += 1;
 }
 
 fn u(v: &i32) -> i32 {
