311aa1449d2553f5811c1584777ba2a233dd9e42
Greg Wiley <gwiley@cj.com>
2019-04-01 14:48:29 -0700

    more fun

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
 
 
+
+#[derive(Debug, Clone)]
+struct Q {
+    x: i32,
+}
+
+
 fn main() {
-    let mut a = 100;
-    t(&mut a);
-    println!("{}", a);
-    println!("{}", u(&a));
 
-    let b1 = String::from("left");
-    let b2 = String::from("right");
-    let c = b1 + &b2;
+    let a = Q{ x:100 };
+    // {
+        let mut b = a;
+        b.x += 100;
+        println!("{:?}", b);
+    // }
+    println!("{:?}", a);
 
-    println!("{}", c);
-
-    panic!("this don't work");
-
-}
-
-fn t( v:  &mut i32) {
-    *v += 1;
-}
-
-fn u(v: &i32) -> i32 {
-    v + 1
 }
